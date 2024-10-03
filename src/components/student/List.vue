6d781977ec7200f1f844e14a0f9f0c2f3227eb09
Lokraj <lokrajthapa741@gmail.com>
2024-10-03 15:23:59 +0545

    ui design for crud operation

---
+++ src/components/student/List.vue
@@ -0,0 +1,71 @@
+<script setup>
+import {
+  EyeIcon,
+  PencilIcon,
+  TrashIcon,
+  UserPlusIcon,
+} from "@heroicons/vue/24/solid";
+import { RouterLink } from "vue-router";
+
+const deleteStudent = async(id)=>{
+    if(!window.confirm("Are you sure you want to delete this student?")){
+        return;
+    }
+    console.log("Deleted", id);
+
+}
+
+</script>
+
+<template>
+  <div>
+    <div class="bg-orange-600 p-4 grid grid-cols-9">
+      <div class="col-span-6 md:col-span-8">
+        <h1 class="text-3xl font-bold text-center mt-3 text-white">
+          Student List
+        </h1>
+      </div>
+      <div class="text-right">
+        <RouterLink :to="{name: 'add'}">
+          <button
+            class="text-white text-md bg-green-700 hover:bg-green-800 font-medium rounded-lg p-2 px-6"
+          >
+            <UserPlusIcon /> Add
+          </button>
+        </RouterLink>
+      </div>
+    </div>
+
+    <table class="table-auto w-full">
+      <thead class="bg-slate-600 text-white">
+        <tr>
+          <th class="py-1">No</th>
+          <th class="py-1">Name</th>
+          <th class="py-1">Email</th>
+          <th class="py-1">Action</th>
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        <tr>
+          <td class="py-2">1</td>
+          <td class="py-2">lokraj</td>
+          <td class="py-2">lokraj@gmail.com</td>
+          <td class="py-2">
+            <RouterLink :to="{name: 'view', params:{id: 1}}">
+                 <EyeIcon class="text-blue-500 h-6 w-6 inline" />
+            </RouterLink>
+            <RouterLink :to="{name: 'edit', params:{id: 1}}">
+                <PencilIcon class="text-emerald-500 h-6 w-6 inline mx-6" />
+            </RouterLink>
+           
+            <TrashIcon class="text-red-500 h-6 w-6 inline cursor-pointer" @click="deleteStudent(1)" />
+
+        
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/student/View.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+
+</script>
+
+<template>
+
+<div class="shadow-md pb-6">
+    <!-- <div
+      class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg font-medium"
+      role="alert"
+      v-if="error"
+    >
+      Oops! Error encountered: {{ error.message }}
+    </div> -->
+    <table class="table-auto w-full">
+      <thead class="bg-slate-600 text-white">
+        <tr>
+          <th class="py-1">ID</th>
+          <th class="py-1">Name</th>
+          <th class="py-1">Email</th>
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        <tr>
+          <td class="py-2">1</td>
+          <td class="py-2">lokraj</td>
+          <td class="py-2">lokraj@gmail.com</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="m-8 flex justify-center">
+      <RouterLink :to="{ name: 'list' }">
+        <button
+          type="button"
+          class="bg-emerald-700 text-white font-medium p-2 rounded-md hover:bg-emerald-800"
+        >
+          Back to Home
+        </button>
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
